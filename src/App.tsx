770d3d53a9185f1cdb7d9810efef2ca9bacdd30f
Namae Conde <namaeconde@gmail.com>
2025-07-26 23:57:59 +1000

    Add initial screen for instructions and set players

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,78 @@
-import { Grid } from "./components/grid.component";
 import { useState } from "react";
-import { Characters, Locations, MARKER, Weapons } from "./lib/types";
+import { Sheet } from "./components/sheet";
+
+interface InitScreenProps {
+    players: string[],
+    setPlayers: Function,
+    startGame: Function
+}
+function InitScreen({ players, setPlayers, startGame }: InitScreenProps) {
+    const [numOfPlayers, setNumOfPlayers] = useState(0);
+    const handleSetNumOfPlayers = (e: any) => {
+        const num = Number(e.target.value);
+        setNumOfPlayers(num);
+        if (num > 0) {
+            const initialPlayers = new Array(num).fill("");
+            setPlayers([...initialPlayers]);
+        } else {
+            setPlayers([]);
+        }
+    }
+
+    return (
+        <div className="init-screen">
+            <div className="info">
+                <span>This is an online note taking sheet in playing the classic Cluedo board game.</span>
+                <span>How to use this Cluedo Sheet:</span>
+                <ol>
+                    <li>Set the number of players of the game you're playing.</li>
+                    <li>Set the initials of the players to be added in your Cluedo Sheet.</li>
+                    <li>Click "Start" to open your sheet.</li>
+                    <li>Click the cells of the sheet to mark with the following indicators:
+                        <ul>
+                            <li>"/" - You have already seen the clue so it will automatically be ruled out in the sheet.</li>
+                            <li>"X" - You are certain the clue is not owned by the player.</li>
+                            <li>"?" - You suspect the clue is owned by the player but you have not seen it yet.</li>
+                        </ul>
+                    </li>
+                </ol>
+
+            </div>
+            <label>How many players?</label>
+            <input type="number" value={numOfPlayers}
+                   onChange={(e) => handleSetNumOfPlayers(e)}/>
+            {
+                players?.length > 0 &&
+                <div className="init-screen">
+                    <label>Set players initials (one letter only)</label>
+                    {
+                        players.map((v, i) => {
+                            return <input key={i} type="text"
+                                          value={v}
+                                          maxLength={1}
+                                          onChange={(e) => {
+                                              players[i] = e.target.value;
+                                              setPlayers([...players])
+                                          }}></input>
+                        })
+                    }
+                    <button onClick={() => {startGame()}}>Start</button>
+                </div>
+            }
+        </div>
+    )
+}
 
 export default function  App() {
-    const [players, setPlayers] = useState([
-        "A",
-        "B",
-        "C",
-        "D",
-        "E"
-    ]);
-    const whoGridItems: { [index: string]: string[] } = {};
-    Object.values(Characters).map((key) => {
-        whoGridItems[key] = new Array(players.length).fill(MARKER.nil);
-    });
+    const [players, setPlayers] = useState<string[]>([]);
+    const [isInProgress, setIsInProgress] = useState(false);
 
-    const whatGridItems: { [index: string]: string[] } = {};
-    Object.values(Weapons).map((key) => {
-        whatGridItems[key] = new Array(players.length).fill(MARKER.nil);
-    });
-
-    const whereGridItems: { [index: string]: string[] } = {};
-    Object.values(Locations).map((key) => {
-        whereGridItems[key] = new Array(players.length).fill(MARKER.nil);
-    });
-    
     return (
         <div>
-            <div style={{ textAlign: "center" }}><b>MY CLUEDO SHEET</b></div>
-            <div style={{ marginBottom: 0 }} className="grid">
-                <div className="grid-row">
-                    <div style={{ display:"flex", gap: "12px" }}>
-                        <div className="grid-row--title"></div>
-                        {players.map((val) => {
-                            return <div style={{ width: "16px", height: "16px" }} key={val}>{val}</div>
-                        })}
-                    </div>
-                </div>
-            </div>
-            <Grid title="WHO" items={whoGridItems} />
-            <Grid title="WHAT" items={whatGridItems} />
-            <Grid title="WHERE" items={whereGridItems} />
+            {
+                !isInProgress ? <InitScreen players={players} setPlayers={setPlayers} startGame={() => setIsInProgress(true)} /> :
+                    <Sheet players={players} />
+            }
         </div>
     )
 }
---
+++ src/components/sheet.tsx
@@ -0,0 +1,42 @@
+import { Characters, Locations, MARKER, Weapons } from "../lib/types";
+import { Grid } from "./grid.component";
+
+interface SheetProps {
+    players: string[]
+}
+
+export const Sheet = ({ players }: SheetProps) => {
+    const whoGridItems: { [index: string]: string[] } = {};
+    Object.values(Characters).map((key) => {
+        whoGridItems[key] = new Array(players.length).fill(MARKER.nil);
+    });
+
+    const whatGridItems: { [index: string]: string[] } = {};
+    Object.values(Weapons).map((key) => {
+        whatGridItems[key] = new Array(players.length).fill(MARKER.nil);
+    });
+
+    const whereGridItems: { [index: string]: string[] } = {};
+    Object.values(Locations).map((key) => {
+        whereGridItems[key] = new Array(players.length).fill(MARKER.nil);
+    });
+
+    return (
+        <div>
+            <div style={{ textAlign: "center" }}><b>MY CLUEDO SHEET</b></div>
+            <div style={{ marginBottom: 0 }} className="grid">
+                <div className="grid-row">
+                    <div style={{ display:"flex", gap: "12px" }}>
+                        <div className="grid-row--title"></div>
+                        {players.map((val) => {
+                            return <div style={{ width: "16px", height: "16px" }} key={val}>{val}</div>
+                        })}
+                    </div>
+                </div>
+            </div>
+            <Grid title="WHO" items={whoGridItems} />
+            <Grid title="WHAT" items={whatGridItems} />
+            <Grid title="WHERE" items={whereGridItems} />
+        </div>
+    )
+}
